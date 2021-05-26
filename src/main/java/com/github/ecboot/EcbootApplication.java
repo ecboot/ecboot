914865e682e60b08271ba8d0781ed807f49337b9
deno <inbox@email.com>
2021-05-26 13:38:47 +0800

    project init

---
+++ src/main/java/com/github/ecboot/EcbootApplication.java
@@ -0,0 +1,13 @@
+package com.github.ecboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcbootApplication.class, args);
+    }
+
+}
