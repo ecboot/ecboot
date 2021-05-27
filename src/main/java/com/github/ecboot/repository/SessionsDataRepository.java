018146fa12d76b4caa9413e49395d1356c7bf3aa
deno <inbox@email.com>
2021-05-27 09:15:23 +0800

    remove session

--- src/main/java/com/github/ecboot/repository/SessionsDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.ecboot.repository;
-
-import com.github.ecboot.entity.SessionsData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionsDataRepository extends JpaRepository<SessionsData, Long> {
-
-}
--- src/main/java/com/github/ecboot/repository/SessionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.ecboot.repository;
-
-import com.github.ecboot.entity.Sessions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionsRepository extends JpaRepository<Sessions, Long> {
-
-}
