7500f39f28047f95b0d6158752169f65fbff5f39
deno <inbox@email.com>
2021-05-27 11:32:44 +0800

    add mybatis-plus

---
+++ src/main/java/com/github/ecboot/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.github.ecboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.ecboot.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
